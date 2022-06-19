e9b38fd3b42da5419405e56f68f99120b1388081
Johan Callens <weylin030@protonmail.ch>
2022-06-19 16:55:32 +0200

    repush

--- components/connect.php
+++ components/connect.php
@@ -1,7 +1,7 @@
 <?php
 $db_name = 'mysql:host=localhost;dbname=shop_db';
-$user_name = 'root';
-$user_password = '111';
+$user_name = '';
+$user_password = '';
 
 $conn = new PDO($db_name, $user_name, $user_password);
 ?>
